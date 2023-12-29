64a6d9fa1a1065c7b8cc8a4c12050e4a3b1af945
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-12-29 15:34:30 +0530

    static file

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from .views import stripe_webhook
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +15,5 @@ urlpatterns = [
     path('api/tickets/', include('tickets.urls')),
     path('api/stripe/webhook/', stripe_webhook, name='stripe-webhook'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
